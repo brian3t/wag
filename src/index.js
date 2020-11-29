d3b73ea4c0dbbc886fae99102ddff1dbdf5101ae
brian3t <ngxtri@gmail.com>
2020-11-28 21:34:09 -0800

    Add a constructor to the Board and set the Board’s initial state to contain an array of 9 nulls corresponding to the 9 squares

--- src/index.js
+++ src/index.js
@@ -3,21 +3,38 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
+    render(){
         return (
-            <button className="square">
-                {/* TODO */}
+            <button className="square"
+                    onClick={() => {
+                this.setState({value : 'X'})
+                    }}>
+                {this.state.value}
             </button>
         );
     }
 }
 
 class Board extends React.Component {
-    renderSquare(i) {
-        return <Square />;
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
     }
 
-    render() {
+    renderSquare(i){
+        return <Square value={i}/>;
+    }
+
+    render(){
         const status = 'Next player: X';
 
         return (
@@ -44,11 +61,11 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
-    render() {
+    render(){
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board/>
                 </div>
                 <div className="game-info">
                     <div>{/* status */}</div>
@@ -62,6 +79,6 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(
-    <Game />,
+    <Game/>,
     document.getElementById('root')
 );
