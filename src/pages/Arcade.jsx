9f5858efb6625063688872e57d0def14958fb24f
brian3t <ngxtri@gmail.com>
2020-12-05 07:40:12 -0800

    f7 react

---
+++ src/pages/Arcade.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {Block,} from 'framework7-react';
+
+import AppstorePage from '../components/AppstorePage';
+
+const Arcade = () => {
+  return (
+    <AppstorePage title="Arcade">
+      <Block>Work in progress</Block>
+    </AppstorePage>
+  );
+};
+
+export default Arcade;
